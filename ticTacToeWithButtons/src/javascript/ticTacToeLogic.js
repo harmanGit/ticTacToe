7335030b34480d2208094963effaa86399309c83
harmanGit <harmandhi22@gmail.com>
2018-11-26 23:06:00 -0500

    works, minor issues

--- ticTacToeWithButtons/src/javascript/ticTacToeLogic.js
+++ ticTacToeWithButtons/src/javascript/ticTacToeLogic.js
@@ -1,15 +1,25 @@
-function start() {
-    document.getElementById("enterButton").onclick = function () {
-        playGame()
-    };
-}
+var moveCounter = 0;
 
 function buttonPressed(id) {
     document.getElementById("btn" + id).innerText = "X";
     document.getElementById("btn" + id).setAttribute("value", "x");
     document.getElementById("btn" + id).disabled = true;
+    moveCounter++;
     playGame();
+}
 
+function buttonPlayGame() {
+    //randomly picking who goes first player or AI
+    if (Math.floor(Math.random() * 2) + 1 === 1) {
+        document.getElementById("results").innerText = "AI Turn!";
+        crazySmartAI(setupBoard()); //AI move
+    } else
+        document.getElementById("results").innerText = "Your Turn!";
+
+    document.getElementById("playGame").disabled = true;
+    document.getElementById("playGame").hidden = true;
+
+    enableAllButtons();
 }
 
 function playGame() {
@@ -21,11 +31,20 @@ function playGame() {
     if (isGameOver != "No Winner :(") {
         document.getElementById("results").innerText = isGameOver;
         disableAllButtons();
-    }
-    //make ai move
-    //check if ai has one
-}
+    } else {
+        //make ai move
+        crazySmartAI(gameBoard);
+        // setting up the new gameBoard
+        gameBoard = setupBoard();
 
+        //checking to see if the Ai has won
+        isGameOver = gameResults(gameBoard, "o");
+        if (isGameOver != "No Winner :(") {
+            document.getElementById("results").innerText = isGameOver;
+            disableAllButtons();
+        }
+    }
+}
 
 function setupBoard() {
     var gameBoardArray = [
@@ -50,18 +69,20 @@ function setupBoard() {
                 default:
                     break;
             }
-
             buttonCounter++;
         }
     }
-
     return gameBoardArray;
 }
 
-function disableAllButtons()
-{
+function disableAllButtons() {
     for (var i = 0; i < 9; ++i)
         document.getElementById("btn" + i).disabled = true;
+}
+
+function enableAllButtons() {
+    for (var i = 0; i < 9; ++i)
+        document.getElementById("btn" + i).disabled = false;
 }
 
 function gameResults(gameBoard, player) { //BUG: TESTED
@@ -71,30 +92,20 @@ function gameResults(gameBoard, player) { //BUG: TESTED
 
     if (gameBoard[0][0] === player && gameBoard[1][1] === player && gameBoard[2][2] === player)// \ check
         results = "won";
-    if (gameBoard[2][2] === player && gameBoard[1][1] === player && gameBoard[0][0] === player)// / check
+    if (gameBoard[0][2] === player && gameBoard[1][1] === player && gameBoard[2][0] === player)// / check
         results = "won";
 
     if (results === "No Winner :(") {
-        mainLoop:
-            for (var r = 0; r < 3; ++r) {
-                //horizontal check
-                    if (gameBoard[r][0] === player && gameBoard[r][1] === player && gameBoard[r][2] === player)
-                    {
-                        results = "won";
-                        break mainLoop;
-                    }
-                //vertical check
-                    if (gameBoard[0][r] === player && gameBoard[1][r] === player && gameBoard[2][r] === player)
-                    {
-                        results = "won";
-                        break mainLoop;
-                    }
-                    drawCounter++;
-            }
+        for (var r = 0; r < 3; ++r) {
+            //horizontal check
+            if (gameBoard[r][0] === player && gameBoard[r][1] === player && gameBoard[r][2] === player)
+                results = "won";
+            //vertical check
+            if (gameBoard[0][r] === player && gameBoard[1][r] === player && gameBoard[2][r] === player)
+                results = "won";
+            drawCounter++;
+        }
     }
-
-    if (drawCounter == 9)
-        return "Game is a Draw";
 
     if (results === "won") {
         if (player === "x")
@@ -102,28 +113,45 @@ function gameResults(gameBoard, player) { //BUG: TESTED
         else if (player === "o")
             return "AI Wins";
     }
+
+    if (moveCounter === 9)
+        return "Game is a Draw";
     return results;
 }
 
-
-function crazySmartAI(openSpotsArray) //BUG: COMPLETE THIS
+function crazySmartAI(gameBoard) //BUG: COMPLETE THIS
 {
-    var openSpacesArray;
+    var freeSpaceCounter = 0;
+    var openSpacesArrayList = [];
     for (var r = 0; r < 3; ++r) {
         for (var c = 0; c < 3; ++c) {
-            if (gameBoard[r][c] === "-") {
-                openSpacesArray.key = r;
-                openSpacesArray[r] = c; //BUG: better data structure. You need to store open positions or something 
+            if (gameBoard[r][c] === "na") {
+                openSpacesArrayList.push(freeSpaceCounter);
             }
+            freeSpaceCounter++;
         }
     }
 
-    var aiMove = Math.floor(Math.random() * openSpacesArray.size);
+    //checking if any moves are left
+    if (openSpacesArrayList.length != 0) {
+        var aiMove = Math.floor(Math.random() * openSpacesArrayList.length);
 
-    //place random move
+        //place random move
+        var id = openSpacesArrayList[aiMove];
+        document.getElementById("btn" + id).innerText = "O";
+        document.getElementById("btn" + id).setAttribute("value", "o");
+        document.getElementById("btn" + id).disabled = true;
 
-    document.getElementById("enterButton").disabled = true;
-    document.getElementById("displayOutput").innerHTML = "You Win";
+        moveCounter++;
+    }
+}
+
+function playAgain() {
+    if (confirm("Press \"Ok\" to play again, else press \"Cancel!\"")) {
+        location.reload();
+    } else {
+        //redirect to homepage
+    }
 }
 
 //Use a random generator to determine whether the machine or the human
