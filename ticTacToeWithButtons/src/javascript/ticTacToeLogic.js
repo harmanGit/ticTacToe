1689716bdd747c302d77b6a039699af876d4a69a
harmanGit <harmandhi22@gmail.com>
2018-11-29 11:16:59 -0500

    Completed, have other people test it

--- ticTacToeWithButtons/src/javascript/ticTacToeLogic.js
+++ ticTacToeWithButtons/src/javascript/ticTacToeLogic.js
@@ -1,6 +1,9 @@
-//BUG READ THE DIRECTIONS AGAIN BRO
-var moveCounter = 0;
+var moveCounter = 0; //total moves made
 
+/**
+ * Onclick for the 9 game buttons
+ * @param int representing the buttons id
+ */
 function buttonPressed(id) {
     document.getElementById("btn" + id).innerText = "X";
     document.getElementById("btn" + id).setAttribute("value", "x");
@@ -9,43 +12,69 @@ function buttonPressed(id) {
     playGame();
 }
 
+/**
+ * Onclick for the play game button, also randomly picks who goes first
+ */
 function buttonPlayGame() {
+    //game starts with buttons disabled, enabling them after game starts
     disableAllButtons(false);
 
     //randomly picking who goes first player or AI
     if (Math.floor(Math.random() * 2) === 1) {
-        document.getElementById("results").innerText = "AI Turn!";
+        document.getElementById("results").innerText = "AI Made A Move!";
         crazySmartAI(getGameBoard()); //AI move
     } else
         document.getElementById("results").innerText = "Your Turn!";
 
+    document.getElementById("results").hidden = false;
     document.getElementById("playGame").disabled = true;
     document.getElementById("playGame").hidden = true;
 }
 
-function buttonPlayAgain() {
-    location.reload()
-}
-
-function disableAllButtons(disable) {
-    for (var i = 0; i < 9; ++i)
-        document.getElementById("btn" + i).disabled = disable;
-}
-
+/**
+ * Function used to play the game
+ */
 function playGame() {
-    if (!gameResults(getGameBoard(), "x")) {//making a move is user didnt win
-        crazySmartAI(getGameBoard());//make ai move
-        gameResults(getGameBoard(), "o")//checking if AI has won
+    if (!gameResults(getGameBoard(), "x")) {//checking to see if the user has won
+        crazySmartAI(getGameBoard());//AI making a move
+        gameResults(getGameBoard(), "o")//checking to see if the AI has won
     }
 }
 
-function playAgain() {
-    document.getElementById("playAgain").disabled = false;
-    document.getElementById("playAgain").hidden = false;
-    document.getElementById("leaveGame").disabled = false;
-    document.getElementById("leaveGame").hidden = false;
+/**
+ * Function finds all open spaces and randomly places a O representing the AI.
+ * @param gameBoard 2d array representing the game board
+ */
+function crazySmartAI(gameBoard) {
+    var freeSpaceCounter = 0;//number of open spaces on the board
+    var openSpacesArrayList = [];
+    for (var r = 0; r < 3; ++r) {
+        for (var c = 0; c < 3; ++c) {
+            if (gameBoard[r][c] === "na") {
+                openSpacesArrayList.push(freeSpaceCounter);
+            }
+            freeSpaceCounter++;
+        }
+    }
+
+    //checking if any moves are left
+    if (openSpacesArrayList.length != 0) {
+        var aiMove = Math.floor(Math.random() * openSpacesArrayList.length);
+
+        //placing random move
+        var id = openSpacesArrayList[aiMove];
+        document.getElementById("btn" + id).innerText = "O";
+        document.getElementById("btn" + id).setAttribute("value", "o");
+        document.getElementById("btn" + id).disabled = true;
+        moveCounter++;
+    }
 }
 
+/**
+ * Function creates a 3 by 3 grid (2d array). Based off the html 9 game buttons, function
+ * places user and computer moves into the array and returns it.
+ * @returns {string[][]} representing the gameboard in array form
+ */
 function getGameBoard() {
     var gameBoardArray = [
         ["na", "na", "na"],
@@ -54,7 +83,7 @@ function getGameBoard() {
     ];
 
     var buttonCounter = 0;
-    for (var r = 0; r < 3; ++r) {
+    for (var r = 0; r < 3; ++r) {//finding the button values for the 2d return array
         for (var c = 0; c < 3; ++c) {
 
             var cellValue = document.getElementById("btn" + buttonCounter).value.toLowerCase();
@@ -75,6 +104,12 @@ function getGameBoard() {
     return gameBoardArray;
 }
 
+/**
+ * Function checks to see if anyone has won or if the game was a draw.
+ * @param gameBoard 2d representing the game board
+ * @param player string representing player, x or o
+ * @returns {boolean} true if the game end else false
+ */
 function gameResults(gameBoard, player) {
     var results = false;
 
@@ -85,10 +120,10 @@ function gameResults(gameBoard, player) {
 
     if (results === false) {
         for (var r = 0; r < 3; ++r) {
-            //horizontal check
+            //horizontal check for all rows
             if (gameBoard[r][0] === player && gameBoard[r][1] === player && gameBoard[r][2] === player)
                 results = true;
-            //vertical check
+            //vertical check for all rows
             if (gameBoard[0][r] === player && gameBoard[1][r] === player && gameBoard[2][r] === player)
                 results = true;
         }
@@ -96,52 +131,58 @@ function gameResults(gameBoard, player) {
 
     if (results === true) {
         if (player === "x")
-            gameOver("Human Wins");
+            gameOver("Human Wins");//calling gameOver function
         else if (player === "o")
             gameOver("AI Wins");
         return results;
     }
-    
 
     if (moveCounter === 9)
         gameOver("Game is a Draw");
     return results;
 }
 
+/**
+ * Function only used when game is over, displays results, disables all game buttons, and calls the playAgain()
+ * @param player string representing the player, either AI ("o") or player ("x")
+ */
 function gameOver(player) {
     document.getElementById("results").innerText = player;
     disableAllButtons(true);
     playAgain();
 }
 
-function crazySmartAI(gameBoard) {
-    var freeSpaceCounter = 0;
-    var openSpacesArrayList = [];
-    for (var r = 0; r < 3; ++r) {
-        for (var c = 0; c < 3; ++c) {
-            if (gameBoard[r][c] === "na") {
-                openSpacesArrayList.push(freeSpaceCounter);
-            }
-            freeSpaceCounter++;
-        }
-    }
-
-    //checking if any moves are left
-    if (openSpacesArrayList.length != 0) {
-        var aiMove = Math.floor(Math.random() * openSpacesArrayList.length);
-
-        //place random move
-        var id = openSpacesArrayList[aiMove];
-        document.getElementById("btn" + id).innerText = "O";
-        document.getElementById("btn" + id).setAttribute("value", "o");
-        document.getElementById("btn" + id).disabled = true;
-        moveCounter++;
-    }
+/**
+ * Function is used for the play again sequence. Displays and enables the play again button
+ */
+function playAgain() {
+    document.getElementById("playAgain").disabled = false;
+    document.getElementById("playAgain").hidden = false;
 }
 
-function load(){
+/**
+ * Onclick for the play gain button, reloads the page
+ */
+function buttonPlayAgain() {
+    location.reload()
+}
+
+/**
+ * Function used to disable or enable the 9 game buttons
+ * @param disable boolean, true to disable and false to enable
+ */
+function disableAllButtons(disable) {
+    for (var i = 0; i < 9; ++i)
+        document.getElementById("btn" + i).disabled = disable;
+}
+
+/**
+ * Function is called as soon as the page loads, displays a pops up with directions
+ */
+function load() {
     window.alert("Directions: Tic-tac-toe is a game where two opponent alternate marking spaces in a 3×3 grid," +
-    " with X's, and O's. The first player who places three of their marks in a row vertical," +
-    " horizontal, or diagonal win the game.");}
+        " with X's, and O's. The first player who places three of their marks in a row vertical," +
+        " horizontal, or diagonal win the game.");
+}
 
 window.addEventListener("load", load);
--- ticTacToeWithMouse/src/javascript/ticTacToeLogic.js
+++ ticTacToeWithMouse/src/javascript/ticTacToeLogic.js
@@ -1,17 +1,16 @@
-//BUG READ THE DIRECTIONS AGAIN BRO
-var moveCounter = 0;
-var divCellArray = new Array(9);
-var gameBoardArray;
-var canPlayGame = false;
+var moveCounter = 0; //total moves made
+var divCellArray = new Array(9); //array of div elements
+var gameBoardArray; //2d array representing the game board with moves from the AI and player
+var canPlayGame = false; //used to disable mouse clicks when the game hasn't started
 
 function mousePressed(mouseEvent) {
 
-    if(canPlayGame){
-        var clientX = mouseEvent.clientX;
-        var clientY = mouseEvent.clientY;
-        var id = validCell(clientX, clientY);
+    if (canPlayGame) { //only make a move if the game has started
+        var clientX = mouseEvent.clientX;// mouses x
+        var clientY = mouseEvent.clientY;// mouses y
+        var id = validCell(clientX, clientY);//checking if the mouse click is in a div, returns div id
 
-        if (checkValidMove(id, "x")) {
+        if (checkValidMove(id, "x")) {//checking to see if x can be placed in the div
             document.getElementById("div" + id).innerText = "X";
             document.getElementById("div" + id).setAttribute("value", "x");
             moveCounter++;
@@ -20,6 +19,9 @@ function mousePressed(mouseEvent) {
     }
 }
 
+/**
+ * Onclick for the play game button, also randomly picks who goes first
+ */
 function buttonPlayGame() {
 
     //randomly picking who goes first player or AI
@@ -29,104 +31,27 @@ function buttonPlayGame() {
     } else
         document.getElementById("results").innerText = "Your Turn!";
 
+    document.getElementById("results").hidden = false;
+
     document.getElementById("playGame").disabled = true;
     document.getElementById("playGame").hidden = true;
     canPlayGame = true;
 }
 
-function validCell(clientX, clientY) {
-    //console.log("cX: " + clientX + " cY:" + clientY);
-    for (var i = 0; i < 9; i++) {
-        var myElement = divCellArray[i];
-        var position = myElement.getBoundingClientRect();
-        var topPosition = position.top;
-        var bottomPosition = position.bottom;
-        var leftPosition = position.left;
-        var rightPosition = position.right;
-        if (clientY > topPosition && clientY <= bottomPosition && clientX >= leftPosition && clientX <= rightPosition)
-            return i//console.log("HERE " + i);
-    }
-    return 999;//if value not found for some reason
-}
-
-function buttonPlayAgain() {
-    location.reload()
-}
-
+/**
+ * Function used to play the game
+ */
 function playGame() {
-    if (!gameResults(gameBoardArray, "x")) {//making a move is user didnt win
-        crazySmartAI(gameBoardArray);//make ai move
-        gameResults(gameBoardArray, "o")//checking if AI has won
+    if (!gameResults(gameBoardArray, "x")) {//checking to see if the user has won
+        crazySmartAI(gameBoardArray);//AI making a move
+        gameResults(gameBoardArray, "o")//checking to see if the AI has won
     }
 }
 
-function load() {
-    window.alert("Directions: Tic-tac-toe is a game where two opponent alternate marking spaces in a 3×3 grid," +
-        " with X's, and O's. The first player who places three of their marks in a row vertical," +
-        " horizontal, or diagonal win the game.");
-
-    loadGameCells();
-
-    gameBoardArray = [
-        ["na", "na", "na"],
-        ["na", "na", "na"],
-        ["na", "na", "na"]
-    ];
-}
-
-function loadGameCells() {
-    for (var i = 0; i < 9; ++i)
-        divCellArray[i] = document.getElementById("div" + i);
-}
-
-function gameResults(gameBoard, player) {
-    var results = false;
-
-    if (gameBoard[0][0] === player && gameBoard[1][1] === player && gameBoard[2][2] === player)// \ check
-        results = true;
-    if (gameBoard[0][2] === player && gameBoard[1][1] === player && gameBoard[2][0] === player)// / check
-        results = true;
-
-    if (results === false) {
-        for (var r = 0; r < 3; ++r) {
-            //horizontal check
-            if (gameBoard[r][0] === player && gameBoard[r][1] === player && gameBoard[r][2] === player)
-                results = true;
-            //vertical check
-            if (gameBoard[0][r] === player && gameBoard[1][r] === player && gameBoard[2][r] === player)
-                results = true;
-        }
-    }
-
-    if (results === true) {
-        if (player === "x")
-            gameOver("Human Wins");
-        else if (player === "o")
-            gameOver("AI Wins");
-        return results;
-    }
-
-    if (moveCounter === 9)
-        gameOver("Game is a Draw");
-
-    console.log(gameBoardArray);
-    return results;
-}
-
-function playAgain() {
-    document.getElementById("playAgain").disabled = false;
-    document.getElementById("playAgain").hidden = false;
-
-}
-
-function gameOver(results) {
-    console.log("gameover");
-    document.getElementById("results").innerText = results;
-    playAgain();
-    canPlayGame = false;
-
-}
-
+/**
+ * Function finds all open spaces and randomly places a O representing the AI.
+ * @param gameBoard 2d array representing the game board
+ */
 function crazySmartAI(gameBoard) {
     var freeSpaceCounter = 0;
     var openSpacesArrayList = [];
@@ -154,6 +79,80 @@ function crazySmartAI(gameBoard) {
     }
 }
 
+/**
+ * Function checks to see if anyone has won or if the game was a draw.
+ * @param gameBoard 2d representing the game board
+ * @param player string representing player, x or o
+ * @returns {boolean} true if the game end else false
+ */
+function gameResults(gameBoard, player) {
+    var results = false;
+
+    if (gameBoard[0][0] === player && gameBoard[1][1] === player && gameBoard[2][2] === player)// \ check
+        results = true;
+    if (gameBoard[0][2] === player && gameBoard[1][1] === player && gameBoard[2][0] === player)// / check
+        results = true;
+
+    if (results === false) {
+        for (var r = 0; r < 3; ++r) {
+            //horizontal check for all rows
+            if (gameBoard[r][0] === player && gameBoard[r][1] === player && gameBoard[r][2] === player)
+                results = true;
+            //vertical check for all rows
+            if (gameBoard[0][r] === player && gameBoard[1][r] === player && gameBoard[2][r] === player)
+                results = true;
+        }
+    }
+
+    if (results === true) {
+        if (player === "x")
+            gameOver("Human Wins");
+        else if (player === "o")
+            gameOver("AI Wins");
+        return results;
+    }
+
+    if (moveCounter === 9)
+        gameOver("Game is a Draw");
+
+    console.log(gameBoardArray);
+    return results;
+}
+
+/**
+ * Function only used when game is over, displays results, disables all game buttons, and calls the playAgain()
+ * @param player string representing the player, either AI ("o") or player ("x")
+ */
+function gameOver(results) {
+    console.log("gameover");
+    document.getElementById("results").innerText = results;
+    playAgain();
+    canPlayGame = false;
+
+}
+
+/**
+ * Function is used for the play again sequence. Displays and enables the play again button
+ */
+function playAgain() {
+    document.getElementById("playAgain").disabled = false;
+    document.getElementById("playAgain").hidden = false;
+
+}
+
+/**
+ * Onclick for the play gain button, reloads the page
+ */
+function buttonPlayAgain() {
+    location.reload()
+}
+
+/**
+ * funciton checks to see if the move being made is valid(not overwriting another previous move)
+ * @param id int representing the div id
+ * @param player string representing the player, either AI ("o") or player ("x")
+ * @returns {boolean} true if the move is valid, else false
+ */
 function checkValidMove(id, player) {
     var indices = document.getElementById("div" + id).innerHTML.valueOf();
     if (indices != "X" && indices != "O") {
@@ -161,9 +160,56 @@ function checkValidMove(id, player) {
         var cols = indices.charAt(4);
         gameBoardArray[row][cols] = player;
         return true;
-        //console.log(indices + " r" + row + " c"+ cols);
     }
     return false;
+}
+
+/**
+ * Function finds are returns which cell the mouse click was, based of the mouse position
+ * and the position of all the divs
+ * @param clientX mouses clients x coordinates
+ * @param clientY mouses clients y coordinates
+ * @returns {number} id of the div the mouse clicked or 999 if no div was clicked
+ */
+function validCell(clientX, clientY) {
+    for (var i = 0; i < 9; i++) {
+        var myElement = divCellArray[i];
+        var position = myElement.getBoundingClientRect();//object position representing a single div
+        var topPosition = position.top;
+        var bottomPosition = position.bottom;
+        var leftPosition = position.left;
+        var rightPosition = position.right;
+        if (clientY > topPosition && clientY <= bottomPosition && clientX >= leftPosition && clientX <= rightPosition)
+            return i
+    }
+    return 999;//if value not found for some reason
+}
+
+/**
+ * Function is called as soon as the page loads.Function displays a pops up with directions, loads the 9 game divs
+ * elements into the divCellArray for global use, and also loads up a empty game board into the global variable
+ * gameBoardArray.
+ */
+function load() {
+    window.alert("Directions: Tic-tac-toe is a game where two opponent alternate marking spaces in a 3×3 grid," +
+        " with X's, and O's. The first player who places three of their marks in a row vertical," +
+        " horizontal, or diagonal win the game.");
+
+    loadGameCells();//loading the div elements into a array
+
+    gameBoardArray = [
+        ["na", "na", "na"],
+        ["na", "na", "na"],
+        ["na", "na", "na"]
+    ];
+}
+
+/**
+ * Function loads all up all the game div elements into a global array divCellArray
+ */
+function loadGameCells() {
+    for (var i = 0; i < 9; ++i)
+        divCellArray[i] = document.getElementById("div" + i);
 }
 
 window.addEventListener("load", load);
